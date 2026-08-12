735fa7d70be4e2c12fab884a49bdf167d209449d
VitoriaMaglio <vitoriamaglio@gmail.com>
2026-08-12 19:39:39 -0300

    Iniciando projeto para 3 sprint do challenge.

---
+++ src/main/java/com/fiap/clyvocaresc/ClyvocarescApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.clyvocaresc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClyvocarescApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClyvocarescApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/clyvocaresc/ClyvocarescApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.clyvocaresc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClyvocarescApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
